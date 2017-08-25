4c063cf1b6d34f15d8cabcb41bca90164d8e6791
Projects-Veno <31170102+Projects-Veno@users.noreply.github.com>
2017-08-25 17:49:36 +0200

    Update custom1.js

--- js/custom1.js
+++ js/custom1.js
@@ -1,17 +1,1 @@
-$(document).ready(function() {
-
-	$("body").css("display", "none");
-
-    $("body").fadeIn(1000);
-
-	$("a.transition").click(function(event){
-		event.preventDefault();
-		linkLocation = this.href;
-		$("body").fadeOut(1000, redirectPage);
-	});
-
-	function redirectPage() {
-		window.location = linkLocation;
-	}
-
-});
+$(document).ready(function() {$("body").css("display", "none");$("body").fadeIn(1000);$("a.transition").click(function(event){event.preventDefault();linkLocation = this.href;$("body").fadeOut(1000, redirectPage);});function redirectPage() {window.location = linkLocation;}});
